17a9bd6938579796598467161cc1b5cb602b379c
MIN <104954505+wallradon@users.noreply.github.com>
2025-06-23 21:27:35 +0700

    จัดการแสดงผมใหม่

--- Sudoku.java
+++ Sudoku.java
@@ -1,28 +1,28 @@
 public class Sudoku {
     public static void main(String[] args) {
-        int n[][] = {
-            {5,3,4,6,7,8,9,1,2},
-            {6,7,2,1,9,5,3,4,8},
-            {1,9,8,3,4,2,5,6,7},
-            {8,5,9,7,6,1,4,2,3},
-            {4,2,6,8,5,3,7,9,1}, 
-            {7,1,3,9,2,4,8,5,6}, 
-            {9,6,1,5,3,7,2,8,4},
-            {2,8,7,4,1,9,6,3,5}, 
-            {3,4,5,2,8,6,1,7,9}, 
-        };
-
-//        int[][] n = {
-//            {5,3,4,6,7,5,9,1,2},
-//            {6,7,2,1,9,5,6,4,8},
+//        int n[][] = {
+//            {5,3,4,6,7,8,9,1,2},
+//            {6,7,2,1,9,5,3,4,8},
 //            {1,9,8,3,4,2,5,6,7},
-//            {8,5,9,7,9,1,4,2,3},
-//            {4,2,6,8,4,3,7,9,1},
-//            {5,1,3,9,2,4,8,5,6},
+//            {8,5,9,7,6,1,4,2,3},
+//            {4,2,6,8,5,3,7,9,1}, 
+//            {7,1,3,9,2,4,8,5,6}, 
 //            {9,6,1,5,3,7,2,8,4},
-//            {2,8,7,4,8,9,6,3,5},
-//            {3,4,5,2,8,6,1,7,9}
-//        };//low over
+//            {2,8,7,4,1,9,6,3,5}, 
+//            {3,4,5,2,8,6,1,7,9}, 
+//        };
+
+        int[][] n = {
+            {5,3,4,6,7,5,9,1,2},
+            {6,7,2,1,9,5,6,4,8},
+            {1,9,8,3,4,2,5,6,7},
+            {8,5,9,7,9,1,4,2,3},
+            {4,2,6,8,4,3,7,9,1},
+            {5,1,3,9,2,4,8,5,6},
+            {9,6,1,5,3,7,2,8,4},
+            {2,8,7,4,8,9,6,3,5},
+            {3,4,5,2,8,6,1,7,9}
+        };//low over
 //        int[][] n = {
 //            {5,3,4,6,7,8,9,1,2},
 //            {6,7,2,1,9,5,3,4,8},
@@ -35,28 +35,28 @@ public class Sudoku {
 //            {3,4,5,2,8,6,1,7,9},
 //        };//column over
        
-//        System.out.println("Sudoku :");
-//        for( int i = 0; i < n.length; i++){
-//            for(int j = 0; j < n[0].length; j++){
-//                System.out.print(n[i][j]+" ");
-//            };
-//            System.out.println("");
-//        };
-//        //print
-//        for( int i = 0; i < n.length; i++){
-////            System.out.print("i="+i+"\n");
-//            for(int j = 0; j < n[0].length; j++){
-////                System.out.print("J="+j+"\n");
+        System.out.println("Sudoku :");
+        for( int i = 0; i < n.length; i++){
+            for(int j = 0; j < n[0].length; j++){
+                System.out.print(n[i][j]+" ");
+            };
+            System.out.println("");
+        };
+        //print
+        for( int i = 0; i < n.length; i++){
+//            System.out.print("i="+i+"\n");
+            for(int j = 0; j < n[0].length; j++){
+//                System.out.print("J="+j+"\n");
 //                System.out.print("N["+i+"]["+j+"]="+n[i][j]+" ");
-//            }//end for j 
+            }//end for j 
 //            System.out.print("\n");
-//        }//end for i
+        }//end for i
         
         //row
         System.out.println("\nCheck row:");
         boolean hasDuplicaterow = false;
         for (int i = 0; i < n.length; i++) {
-            boolean[] check = new boolean[10]; // index 1–9, ขนาด 10 พอดี  
+            boolean[] check = new boolean[10]; 
 //            System.out.print("i="+i+"\n");
             
 
@@ -65,6 +65,7 @@ public class Sudoku {
                 int value = n[i][j];
                 if (check[value]) {
                     hasDuplicaterow = true;
+                    System.out.print("Duplicate In ");
                     System.out.println("ROW: "+i+" Column : "+j+"");
 //                    System.out.println("Sudoku It game Over");
 //                    return ;
@@ -87,7 +88,8 @@ public class Sudoku {
                 int value = n[i2][j2];
                 if (check2[value]) {
                     hasDuplicateColumn = true;
-                    System.out.println("ROW:"+i2+"Column"+j2+"");
+                    System.out.print("Duplicate In ");
+                    System.out.println("ROW: "+i2+" Column "+j2+"");
 //                    System.out.println("Sudoku It game Over");
 //                    return;
                 }
