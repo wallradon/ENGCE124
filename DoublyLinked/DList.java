565bc8f9e21dbe0b9dfc9f3017c77fb6363366d9
Wallradon Promanan <wallradon@gmail.com>
2025-08-29 00:28:50 +0700

    Build append

--- DoublyLinked/DList.java
+++ DoublyLinked/DList.java
@@ -1,73 +1,17 @@
-
 public class DList {
-    DNode head, tail, travel,position;
+    DNode head,tail,travel ;
     int count;
-    void addFirst(int data){
-        DNode newnode = new DNode(data);
-        if(count==0){
-            append(data);
-        }else{
-            newnode.Rlink = head;
-            head.Llink = newnode;
-            head = newnode;
-        }
-        count++;
-    }
     void append(int data) {
-        DNode newnode = new DNode(data);
-        if (count == 0) {
-            head = newnode;
-            tail = newnode;
-        } else {
-            tail.Rlink = newnode;
-            newnode.Llink = tail;
-            tail = newnode;
+        DNode newnode = new DNode() ;
+        newnode.info = data ;
+        if ( count == 0 ) {
+            head = newnode ; 
+            tail = newnode ;
+        }else{
+            tail.Rlink = newnode ;
+            tail = newnode ;
         }
-        count++;
-    }
-    void addNext(int data,int target){
-        position = search(target);
-        if(position != null){
-            if(position == tail){
-                append(data);
-            }else{
-                DNode newnode = new DNode(data);
-                newnode.Rlink = position.Rlink;
-                newnode.Llink = position;
-                position.Rlink.Llink = newnode;
-                position.Rlink = newnode;
-                count++;
-            }      
-        }
-    }
-    void addPrev(int data,int target){
-        position = search(target);
-        if(position != null){
-            if(position == head){
-                addFirst(data);
-            }else{
-                DNode newnode = new DNode(data);
-                newnode.Rlink = position;
-                newnode.Llink = position.Llink;
-                position.Llink.Rlink = newnode;
-                position.Llink = newnode;
-                count++;
-            }      
-        }
-    }  
-    DNode search(int target) {
-        travel = head;
-        while (travel.info != target && travel != null) {
-            travel = travel.Rlink;
-        }
-        return travel;
-    }  
-    void showAll() {
-        travel = head;
-        while (travel != null) {
-            System.out.println(travel.info); //INFO(travel)
-            travel = travel.Rlink;  //travel = LINK(travel)
-        }
-        System.out.println("----------------");
+
+
     }
 }
