e5a92b263c01790126a951ee00fb911440801754
Wallradon Promanan <wallradon@gmail.com>
2025-09-26 03:53:00 +0700

    learn java gui

---
+++ GUI/GUI.java
@@ -0,0 +1,15 @@
+import javax.swing.* ;
+
+public class GUI {
+    
+    public static void main(String[] args) {
+        JFrame f = new JFrame() ;
+        f.setTitle("Music playlist") ;
+        f.setSize(600, 400) ;
+        f.setResizable(false) ;
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ;
+        f.setVisible(true) ;
+        
+    }
+
+}
